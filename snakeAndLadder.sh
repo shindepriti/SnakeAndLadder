517b71f166357406055dcb54ccdcf6134b5c9984
shindepriti <pritishinde34@gmail.com>
2020-03-09 14:20:48 +0530

    ADDED:Function To Check iIf Payer Position Go Above Maximum Position

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -5,27 +5,34 @@ echo "WEL-COME To Snake And Ladder Game"
 position=0
 
 #CONSTANTS
+START_POSITION=0
 MAX_POSITION=100
 NOPLAY=0
 LADDER=1
 SNAKE=2
 
-randomVariable=$((RANDOM%6+1))
+function getExactPosition(){
+	#if position of player move below zero then restart from zero  
+	if [ $position -lt $START_POSITION ]
+	then
+		position=$START_POSITION
+	fi
+	if [ $position -gt $MAX_POSITION ]
+	then
+		position=$((position-randomVariable))
+	fi
+}
 
 function checkOptions(){
 	while [[ $position -lt $MAX_POSITION ]]
 	do
+		randomVariable=$((RANDOM%6+1))
 		case $((RANDOM%3)) in 
 			$NOPLAY) position=0 ;;
 			$LADDER) position=$((position + $randomVariable)) ;;
 			$SNAKE) position=$((position - $randomVariable)) ;;
 		esac
-		#if position of player move below zero then restart from zero  
-		if [ $position -lt 0 ]
-		then
-			position=0
-		fi
-
+		getExactPosition
 	done
 }
 checkOptions
