ba0b0411a0d9cc042b452384a5419f228a1a1477
shindepriti <pritishinde34@gmail.com>
2020-03-09 18:24:42 +0530

    ADDED:Play Game Between Two Player And Check Winner

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,42 +1,61 @@
-#!/bin/bash -x
-echo "WEL-COME To Snake And Ladder Game"
+#!/bin/bash -x 
+echo "***Wel-Come TO Snake And Laddder Game***"
 
-declare -A playerPosition
+declare -A playerPositions
 #variable
 position=0
 dieCount=0
+player=1
 
 #CONSTANTS
-START_POSITION=0
 MAX_POSITION=100
-NOPLAY=0
-LADDER=1
-SNAKE=2
+START_POSITION=0
+NO_PLAY=1
+LADDER=2
+SNAKE=3	
 
-function getExactPosition(){
-	#if position of player move below zero then restart from zero  
-	if [ $position -lt $START_POSITION ]
-	then
-		position=$START_POSITION
-	fi
-	if [ $position -gt $MAX_POSITION ]
-	then
-		position=$((position-randomVariable))
-	fi
-}
-
-function checkOptions(){
-	while [[ $position -ne $MAX_POSITION ]]
-	do
-		randomVariable=$((RANDOM%6+1))
-		((dieCount++))
-		case $((RANDOM%3)) in 
-			$NOPLAY) position=0 ;;
-			$LADDER) position=$((position + $randomVariable)) ;;
-			$SNAKE) position=$((position - $randomVariable)) ;;
+function checksForOption(){
+#repeate player till winning position
+while [[ $position -ne $MAX_POSITION ]]
+do
+		playerMoves
+		randomVariable=$((RANDOM%6+1))		
+		case $((RANDOM%3+1)) in
+		$NO_PLAY)
+			position=$position ;;
+		$LADDER)
+			position=$((position+randomVariable)) ;;
+		$SNAKE)
+			position=$((position-randomVariable)) ;;
 		esac
 		getExactPosition
-		playerPosition[$dieCount]=$position
-	done
+		playerPositions[$player]=$position
+		((dieCount++))			
+
+done
+	echo "player$player is Win"
 }
-checkOptions
+
+function getExactPosition() {
+# in case player position move below 0 then restart from 0
+if [[ $position -lt $START_POSITION ]]
+then
+	position=0
+fi
+# case if player move above winning position 100
+if [[ $position -gt $MAX_POSITION ]]
+then
+	position=$((position-randomVariable))
+fi
+}
+
+function playerMoves(){
+if [[ $player -eq 1 ]]
+then
+	player=2
+else
+	player=1
+fi
+}
+#game Start Here
+checksForOption
