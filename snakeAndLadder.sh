c7815caece0401a2e82635f4a663ef582d134bcd
shindepriti <pritishinde34@gmail.com>
2020-03-09 12:27:11 +0530

    ADDED:Function To Check For options NoPlay,Ladder And Snake

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -5,3 +5,20 @@ echo "WEL-COME To Snake And Ladder Game"
 position=0
 
 randomVariable=$((RANDOM%6+1))
+
+#CONSTANTS
+NOPLAY=0
+LADDER=1
+SNAKE=2
+
+randomVariable=$((RANDOM%6+1))
+
+function checkOptions(){
+   case $((RANDOM%3)) in 
+      $NOPLAY) position=0 ;;
+      $LADDER) position=$((position + $randomVariable)) ;;
+      $SNAKE) position=$((position - $randomVariable)) ;;
+   esac
+}
+checkOptions
+
