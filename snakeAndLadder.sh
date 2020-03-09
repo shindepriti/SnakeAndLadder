6a3e3424026256080a9d42c35766457d68c222a0
shindepriti <pritishinde34@gmail.com>
2020-03-09 12:42:11 +0530

    ADDED:Repeate Till Winning Position & Below Zero Position Become Zero

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -4,9 +4,8 @@ echo "WEL-COME To Snake And Ladder Game"
 #variable
 position=0
 
-randomVariable=$((RANDOM%6+1))
-
 #CONSTANTS
+MAX_POSITION=100
 NOPLAY=0
 LADDER=1
 SNAKE=2
@@ -14,11 +13,19 @@ SNAKE=2
 randomVariable=$((RANDOM%6+1))
 
 function checkOptions(){
-   case $((RANDOM%3)) in 
-      $NOPLAY) position=0 ;;
-      $LADDER) position=$((position + $randomVariable)) ;;
-      $SNAKE) position=$((position - $randomVariable)) ;;
-   esac
+	while [[ $position -lt $MAX_POSITION ]]
+	do
+		case $((RANDOM%3)) in 
+			$NOPLAY) position=0 ;;
+			$LADDER) position=$((position + $randomVariable)) ;;
+			$SNAKE) position=$((position - $randomVariable)) ;;
+		esac
+		#if position of player move below zero then restart from zero  
+		if [ $position -lt 0 ]
+		then
+			position=0
+		fi
+
+	done
 }
 checkOptions
-
